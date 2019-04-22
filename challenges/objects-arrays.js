9e669595d985f42ceab28cecbd5815fbdb563843
Crystal Martin <drcrystalm@gmail.com>
2019-04-21 21:48:38 -0500

    finished functions

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -160,8 +160,9 @@ const graduates = [{
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities)
+
+const universities = [graduates.university];
+console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -252,6 +253,14 @@ The zoo wants to display both the scientific name and the animal name in front o
 */
 const animalNames = [];
 console.log(animalNames);
+
+
+function printNames(item) {
+  console.log `${this.animal_name}, ${this.scientific_name}.`;
+}
+
+zooAnimals.forEach(printNames);
+
 
 /* Request 2: .map()    
 
